435f0cceaaae6b29560e9c3ff8578ab14aafd02c
Khalid Alexaneder <khalid.alexander2184@gmail.com>
2026-02-04 23:28:24 -0500

    sub

--- src/main/java/SkillBuilder1.java
+++ src/main/java/SkillBuilder1.java
@@ -9,15 +9,23 @@ import java.util.Scanner;
 public class SkillBuilder1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Whats your name?");
+        System.out.println("You're Jane's friend!");
+        System.out.println("\"What's your name?\"");
         String name = input.nextLine();
+
+        System.out.println("Enter a floating-point number:");
+        double spice = input.nextDouble();
+        double spiceAns = (4.0 / 3.0) * Math.pow(2, Math.sqrt(5) / Math.pow(spice, 3));
+
+        System.out.printf("Well %s, the spice value resulted in %.4f", name, spiceAns);
+        double converted = (int)(spiceAns * 100) / 100.0;
+        System.out.println("\nAnd the converted value is " + converted);
+
+
     }
 
     public static void calcWallPaint() {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a floating-point number:");
-        int number = input.nextInt();
-
 
         final double squareFeetPerGallons = 350.0;
         final double gallonsPerCan = 1.0;
@@ -31,9 +39,13 @@ public class SkillBuilder1 {
         double wallWidth = input.nextDouble();
 
         // TODO: Calculate and output the wall's area
-
+        double area = wallHeight * wallWidth;
+        System.out.println("Wall area: " + area + " square feet");
         // TODO: Calculate and output the amount of paint in gallons needed to paint the wall
-
+        double paintNeeded = area / squareFeetPerGallons;
+        System.out.printf("Paint needed: %.2f gallons\n", paintNeeded);
         // TODO: Calculate and output the number of 1 gallon cans needed to paint the wall, rounded up to nearest integer
+        int cansNeeded = (int)Math.ceil(paintNeeded);
+        System.out.println("Cans needed: " + cansNeeded + " can(s)");
     }
 }
